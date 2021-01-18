44b980ee9d976ac505ae884a173a1db66d00a73e
prathick96 <paddhu.xd96@gmail.com>
2021-01-18 15:50:25 +0530

    JSON Dynamic load success

--- src/components/CarouselDemo.js
+++ src/components/CarouselDemo.js
@@ -2,20 +2,19 @@ import React, {useState, useEffect} from "react";
 import { Carousel, Image } from "react-bootstrap";
 
 const CarouselDemo = () => {
-  const [imageFeed] = useState([])
+
+  const [imgFeed, setImgFeed] = useState([])
 
   useEffect(() => {
-    fetch('/data/'+imageFeed+'.json',{mode: 'no-cors'})
+    fetch('/data/imageFeed.json',{mode: 'no-cors'})
     .then(res=>res.json())
-    .then(data=> console.log(data))
+    .then(data=> setImgFeed(data))
     .then(err=> console.error(err))
-  }, [imageFeed])
-
-  console.log(imageFeed)
+  }, [])
 
   return (
     <Carousel className='bg-dark'>
-    {imageFeed.map((image) => (
+    {imgFeed.map((image) => (
       <Carousel.Item key={image._id}>
         <Image src={image.imgSrc} alt={image.imgName} fluid />
       </Carousel.Item>
